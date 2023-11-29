d161378e96969cbd08132d33816f10dd85b662e7
amtulaleem <145044820+amtulaleem@users.noreply.github.com>
2023-11-29 11:04:54 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,82 +1,29 @@
-// Event listener for the button click
-document
-  .getElementById("calculateScore")
-  .addEventListener("click", calculateScore);
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-// Function to calculate and display the quiz score
-function calculateScore() {
-  // Retrieving user inputs and converting to lowercase
-  let capital = document.getElementById("cap").value.toLowerCase();
-  let knownFact = document.getElementById("known").value.toLowerCase();
-  let largestCityAnswer = document
-    .getElementById("largestCity")
-    .value.toLowerCase();
-  let nationalParkAnswer = document
-    .getElementById("nationalPark")
-    .value.toLowerCase();
+function btnClicked() {
+  let randNum = Math.random();
+  let input = document.getElementById("search-in").value;
+  let output = document.getElementById("output");
 
-  // Initialize variables for tracking correct answers
-  let totalQuestions = 4;
-  let correctAnswers = 0;
-
-  // Question 1
-  if (capital === "edmonton") {
-    document.getElementById("res1").innerHTML = "Correct";
-    correctAnswers++;
-  } else {
-    document.getElementById("res1").innerHTML = "Incorrect";
-  }
-
-  // Question 2
-  if (knownFact === "oil") {
-    document.getElementById("res2").innerHTML = "Correct";
-    correctAnswers++;
-  } else {
-    document.getElementById("res2").innerHTML = "Incorrect";
-  }
-
-  // Question 3
-  if (largestCityAnswer === "calgary") {
-    document.getElementById("res3").innerHTML = "Correct";
-    correctAnswers++;
-  } else {
-    document.getElementById("res3").innerHTML = "Incorrect";
-  }
-
-  // Question 4
-  if (
-    nationalParkAnswer === "banff" ||
-    nationalParkAnswer === "banff national park"
+  if (input.value.trim() === "") {
+    output.innerHTML = "Please ask a question...";
+  } else if (
+    input.value.toLowerCase() === "does a magic 8 ball actually work?"
   ) {
-    document.getElementById("res4").innerHTML = "Correct";
-    correctAnswers++;
+    output.innerHTML = "How dare you doubt me!";
+  } else if (input.value.toLowerCase() === "is javascript awesome?") {
+    output.innerHTML = "Of course!";
   } else {
-    document.getElementById("res4").innerHTML = "Incorrect";
+    if (randNum < 0.2) {
+      output.innerHTML = "Outlook not so good.";
+    } else if (randNum < 0.4) {
+      output.innerHTML = "Without a doubt, yes";
+    } else if (randNum < 0.6) {
+      output.innerHTML = "Don't count on it.";
+    } else if (randNum < 0.8) {
+      output.innerHTML = "As I see it, yes.";
+    } else {
+      output.innerHTML = "Concentrate and ask again";
+    }
   }
-
-  // Calculate the percentage
-  let percentage = (correctAnswers / totalQuestions) * 100;
-
-  // Display the total score and percentage
-  document.getElementById("output").innerHTML =
-    "Total Score: " +
-    correctAnswers +
-    "/" +
-    totalQuestions +
-    " (" +
-    percentage +
-    "%)";
-
-  // Provide feedback to the user based on their score
-  let feedbackMessage = "";
-  if (percentage === 100) {
-    feedbackMessage = "Perfect score! Great job!";
-  } else if (percentage >= 75) {
-    feedbackMessage = "Well done! Keep it up!";
-  } else {
-    feedbackMessage = "You can do better. Keep practicing!";
-  }
-
-  // Display the feedback message
-  document.getElementById("feedback").innerHTML = feedbackMessage;
 }
